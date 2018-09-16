a6645d602b80b874f2336242a0cbc8aad20573df
lx kish <urban.ktulhu@gmail.com>
2018-09-17 08:21:55 +1200

    193 Webpack and React

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            This is a react app
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
